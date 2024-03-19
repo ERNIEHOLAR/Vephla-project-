f526a163d747630329eb42da277c6e375b6e2a02
ernie <ogunbanwoeniola.oe@gmail.com>
2024-03-19 16:16:33 +0100

    read more link added

--- file.js
+++ file.js
@@ -1,8 +1,24 @@
-function showMore(){
-    //removes the link
-    document.getElementById('link').parentElement.removeChild('link');
-    //shows the #more
-    document.getElementById('more').style.display = "block";
+//EXPANDABLE READ MORE LINK
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+function changeBg() {
+  var moreText = document.getElementById("more");
+
+  
 }
 
 const menu = document.querySelector("#menu-links");
